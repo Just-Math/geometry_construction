3c8bb5cf3cce22f2d49a7ba85d9e70818ea4c4d3
JustMath <53122254+Just-Math@users.noreply.github.com>
2021-08-28 21:35:53 +0300

    Обновление от 28.08.21
    Обновлены описания функций, исправлены некоторые ошибки. 
    Спасибо пользователю Ветер Дикого-Техаса за помощь!

--- geometric_constructions.py
+++ geometric_constructions.py
@@ -2,36 +2,38 @@ from manim import *
 import numpy as mp
 
 """ Вычисление длин, векторов, углов """
+
 def distance(A, B):
+    """ Расстояние между точками A и B """
     a = np.linalg.norm(B - A)
     return a
 
 def perimeter_triangle(A, B, C):
-    #A, B, C - координаты вершин треугольника с 
-    AB = distance(B, A)
-    BC = distance(C, B)
-    AC = distance(C, A)
+    """ Периметр треугольника ABC """
+    AB = distance(A, B)
+    BC = distance(B, C)
+    AC = distance(A, C)
     P = AB + BC + AC
     return P
 
 def area_triangle(A, B, C):
-    #Возращает значение площади треугольника
-    AB = distance(B, A)
-    BC = distance(C, B)
-    AC = distance(C, A)
+    """ Площадь треугольника ABC """
+    AB = distance(A, B)
+    BC = distance(B, C)
+    AC = distance(A, C)
     p = perimeter_triangle(A, B, C)/2
     S = (p * (p - AB) * (p - BC) * (p - AC)) ** 0.5
     return S
 
 def inradius(A, B, C):
-    #Возвращает радиус вписанной окружности
+    """ Радиус окружности, вписанной в треугольник ABC """
     S = area_triangle(A, B, C)
     p = perimeter_triangle(A, B, C)/2
     r = S / p
     return r
 
 def circumradius(A, B, C):
-    #Возращает радиус описанной окружности
+    """ Радиус окружности, описанной около треугольника ABC """
     a = distance(A, B)
     b = distance(B, C)
     c = distance(A, C)
@@ -40,8 +42,7 @@ def circumradius(A, B, C):
     return R
 
 def exradius(A, B, C):
-    #Возвращает значение радиуса вневписанной окружности
-    #центр которой напротив вершины A
+    """ Радиус вневписанной окружности с центром напротив вершины A """
     S = area_triangle(A, B, C)
     p = perimeter_triangle(A, B, C) / 2
     a = distance(B, C)
@@ -49,17 +50,17 @@ def exradius(A, B, C):
     return R_a
 
 def normal_vector(A, B):
-    #Возращает координаты вектора нормали прямой AB
+    """ Вектор нормали прямой AB """
     n = np.array([B[1] - A[1], A[0] - B[0], 0])
     return n
 
 def direction_vector(A, B):
-    #направляющей вектор прямой AB
-    c = np.array([B[0] - A[0],B[1] - A[1], 0])
-    return c
+    """ Направляющий вектор прямой AB """
+    v = np.array([B[0] - A[0], B[1] - A[1], 0])
+    return v
 
 def angle_cosinus(A, B, C):
-    #Возращает значение косинуса угла ABC
+    """ Косинус угла ABC """
     a = distance(B, C)
     b = distance(C, A)
     c = distance(B, A)   
@@ -67,27 +68,27 @@ def angle_cosinus(A, B, C):
     return cos
 
 def angle_sin(A, B, C):
-    #Возращает значение синуса угла ABC
+    """ Синус угла ABC """
     cos = angle_cosinus(A, B, C)
     sin = (1 - cos ** 2) ** 0.5
     return sin
 
 def angle_tan(A, B, C):
-    #Возвращает тангенс угла ABC
+    """ Тангенс угла ABC """
     cos = angle_cosinus(A, B, C)
     sin = angle_sin(A, B, C)
     tan = sin / cos
     return tan
 
 def angle_cot(A, B, C):
-    #Возвращает котангенс угла ABC
+    """ Котангенс угла ABC """
     cot = 1 / (angle_tan(A, B, C))
     return cot
 
 """Замечательные точки """
 
 def incenter(A, B, C):
-    #Возвращает координаты инцентра
+    """ Координаты центра окружности, вписанной в треугольник ABC """
     a = distance(B, C)
     b = distance(A, C)
     c = distance(A, B)
@@ -95,7 +96,7 @@ def incenter(A, B, C):
     return I
 
 def circumcenter(A, B, C):
-    #Центр окружности, описанной около треугольника
+    """ Координаты центра окружности, описанной около треугольника ABC """
     M1 = midpoint(A, B)
     M2 = midpoint(A, C)
     n1 = normal_vector(A, B)
@@ -104,15 +105,14 @@ def circumcenter(A, B, C):
     return O
 
 def point_to_line_projection(C, A, B):
-    #Возвращает координаты проекции точки C на прямую AB
+    """ Координаты проекции точки C на прямую AB """
     n = normal_vector(A, B)
     a = direction_vector(A, B)
     H = find_intersection(C, n, A, a)
     return H
 
 def eccentric(A, B, C):
-    #Возвращает координаты вневписанной окружности
-    #Напротив вершины A
+    """ Координаты центра вневписанной окружности с центром напротив вершины A """
     a = distance(B, C)
     b = distance(A, C)
     c = distance(A, B)
@@ -120,7 +120,7 @@ def eccentric(A, B, C):
     return I_A
 
 def nagel_point(A, B, C):
-    #Возвращает координаты точки Нагеля
+    """ Координаты точки Нагеля """
     p = perimeter_triangle(A, B, C) / 2
     a = distance(B, C)
     b = distance(A, C)
@@ -129,33 +129,33 @@ def nagel_point(A, B, C):
     return N
 
 def center_mass(A, B, C):
-    #Возращает координаты центра масс треугольника ABC
+    """ Координаты центра масс треугольника ABC """
     Z = (A + B + C) / 3
     return Z
 
 def orthocenter(A, B, C):
-    #Возвращает координаты ортоцентра треугольника
+    """ Координаты ортоцентра треугольника ABC """
     H1 = point_to_line_projection(A, B, C)    
     H2 = point_to_line_projection(B, A, C) 
     H = find_intersection(A, H1 - A, B, H2 - B)   
     return H
 
 def center_O9(A, B, C):
-    #Возвращает центр окружности девяти точек
+    """ Координаты центра окружности девяти точек для треугольника ABC """
     O = circumcenter(A, B, C)
     H = orthocenter(A, B, C)
     O9 = 0.5 * (O + H)
     return O9
 
 def bisectors(A, B, C):
-    #Возвращает основание биссектрисы напротив первой вершины
+    """ Координаты основания биссектрисы угла A треугольника ABC """
     b = distance(A, C)
     c = distance(A, B)
     L = (b * B + c * C)/(b + c)
     return L
 
 def split_segment(A, B, k):
-    #Деление отрезка AB в отношении k
+    """ Координаты точки, которая делит отрезок AB в отношении 1:k, считая от A """
     N = np.array(
         [
             (A[0] + B[0] * k) / (1 + k),
@@ -166,18 +166,17 @@ def split_segment(A, B, k):
     return N
 
 """Оформление чертежа """
+
 def extend_segment(A, B, k):
-    #Продлить отрезок AB на k единиц
-    # за точку B
+    """ Координаты точки, лежащей на продолжении отрезка AB за точку B через k единиц """
     dAB = distance(A, B)
     vAB = direction_vector(A, B) / dAB
     C = B + vAB * k
     return C
 
-def mark_line(A, B, a, width = 0.7, **kwargs):
-    #Отметить отрезок AB штрихом
-    # а - количество штрихов
-    # на месте kwargs - аргументы из класса Line() для задания цвета, прозрачности и т.д.
+def mark_line(A, B, a, width = 1, **kwargs):
+    """ Штрихи, отмечающие отрезок AB
+    а - количество штрихов """
     m = midpoint(A, B)
     n = normal_vector(A, B) / (np.linalg.norm(B - A))
     p = np.array([B[0] - A[0], B[1] - A[1], 0]) / (np.linalg.norm(B - A))
@@ -185,10 +184,10 @@ def mark_line(A, B, a, width = 0.7, **kwargs):
     F1 = m + 0.1 * n
     F2 = m - 0.1 * n
     if a > 0:
-        s1 = Line(F1, F2, stroke_width=width*DEFAULT_STROKE_WIDTH, **kwargs)
+        s1 = Line(F1, F2, stroke_width = width * 0.7 * DEFAULT_STROKE_WIDTH, **kwargs)
         lines.add(s1)
         for i in range(a):
-            s2 = Line(F1, F2, stroke_width=width*DEFAULT_STROKE_WIDTH, **kwargs).shift(i * width / 10 * p)
+            s2 = Line(F1, F2, stroke_width = width * 0.7 * DEFAULT_STROKE_WIDTH, **kwargs).shift(i * width *.07 * p)
             lines.add(s2)
     lines.move_to(m)
     return lines
