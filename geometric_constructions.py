f58760e29db1516e3e2176b739b073c0edf983ed
JustMath <53122254+Just-Math@users.noreply.github.com>
2021-09-05 09:05:41 +0300

    Обновление от 05.09. Добавлены 2 функции
    Добавлены функции "Касательные к окружности" и "Общие касательные к двум окружностям". 
    Автор функций: Love Math(ссылка: clck.ru/X9bbP)

--- geometric_constructions.py
+++ geometric_constructions.py
@@ -165,6 +165,75 @@ def split_segment(A, B, k):
     )
     return N
 
+def tangent_dots(c, A):
+    """ Координаты 2 точек, в которых касательные, проведённые из точки A, касаются окружности c.
+    A лежит вне окружности c """
+   """Автор функции: Love Math (clck.ru/X9bbP)"""
+    c_x, c_y = c.get_x(), c.get_y()
+    A_x, A_y = A[0], A[1]
+    d_x, d_y = A_x - c_x, A_y - c_y
+    r = c.radius
+    if d_x ** 2 + d_y ** 2 > r ** 2:
+        minus_b2 = -d_x * d_y
+        sqrt_D4 = r * np.sqrt(d_x ** 2 + d_y ** 2 - r ** 2)
+        a = r ** 2 - d_x ** 2
+        if abs(a) > 1e-15:
+            k1 = (minus_b2 + sqrt_D4) / a
+            k2 = (minus_b2 - sqrt_D4) / a
+            x1 = (c_x + k1 * (k1 * A_x - d_y)) / (1 + k1 ** 2)
+            x2 = (c_x + k2 * (k2 * A_x - d_y)) / (1 + k2 ** 2)
+            y1 = k1 * (x1 - A_x) + A_y
+            y2 = k2 * (x2 - A_x) + A_y
+        else:
+            a = r ** 2 - d_y ** 2
+            if abs(a) > 1e-15:
+                k1 = (minus_b2 + sqrt_D4) / a
+                k2 = (minus_b2 - sqrt_D4) / a
+                y1 = (c_y + k1 * (k1 * A_y - d_x)) / (1 + k1 ** 2)
+                y2 = (c_y + k2 * (k2 * A_y - d_x)) / (1 + k2 ** 2)
+                x1 = k1 * (y1 - A_y) + A_x
+                x2 = k2 * (y2 - A_y) + A_x
+            else:
+                x1 = c_x
+                y2 = c_y
+                if A_y > c_y:
+                    y1 = c_y + r
+                else:
+                    y1 = c_y - r
+                if A_x > c_x:
+                    x2 = c_x + r
+                else:
+                    x2 = c_x - r
+        return [np.array([x1, y1, 0]), np.array([x2, y2, 0])]
+    else:
+        return (np.array([np.nan, np.nan, np.nan]), np.array([np.nan, np.nan, np.nan]))
+
+def tangent_dots_for_2_circles(c1, c2):
+    """ Координаты 4 точек, в которых общие касательные касаются окружностей c1 и c2.
+    На выходе первые 2 точки в списке - для окружности c1, последние 2 точки - для окружности c2 """
+    """Автор функции: Love Math (clck.ru/X9bbP)"""
+    r1, r2 = c1.radius, c2.radius
+    c1_c, c2_c = c1.get_center(), c2.get_center()
+    if abs(r1 - r2) < 1e-15:
+        normal_c = normal_vector(c1_c, c2_c)
+        dot1 = c1_c + normal_c / np.linalg.norm(normal_c) * r1
+        dot2 = c1_c - normal_c / np.linalg.norm(normal_c) * r1
+        dot3 = c2_c + normal_c / np.linalg.norm(normal_c) * r2
+        dot4 = c2_c - normal_c / np.linalg.norm(normal_c) * r2
+    else:
+        if r1 - r2 <= -1e-15:
+            cs_c, cl_c = c1_c, c2_c
+            rs, rl = r1, r2
+        else:
+            cs_c, cl_c = c2_c, c1_c
+            rs, rl = r2, r1
+        d = distance(c1_c, c2_c)
+        shift = rs * d / (rl - rs)
+        A = extend_segment(cl_c, cs_c, shift)
+        [dot1, dot2] = tangent_dots(c1, A)
+        [dot3, dot4] = tangent_dots(c2, A)
+    return [dot1, dot2, dot3, dot4]
+
 """Оформление чертежа """
 
 def extend_segment(A, B, k):
